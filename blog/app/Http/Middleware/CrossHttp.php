7e51508ee8f13b62d7c2d5ad4983769210be7279
贺小娇 <1234324@qq.com>
2018-03-28 16:18:12 +0800

    添加中间件

--- blog/app/Http/Middleware/CrossHttp.php
+++ blog/app/Http/Middleware/CrossHttp.php
@@ -16,7 +16,7 @@ class CrossHttp
     public function handle($request, Closure $next)
     {
         return $next($request);
-        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Origin', 'http://localhost:8080');
         $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
         // $response->header('Access-Control-Allow-Credentials', 'true');
